bcf4e027f32270767730cc760ce3668bd8899b77
IhorSliepchenko1 <igorslepchenko.cc@gmail.com>
2024-11-07 23:35:00 +0200

    init

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import { Button } from "../Button/Button"
+
+export const Header = () => {
+     return (
+          <div className="header">
+               <Button>Закрыть</Button>
+          </div>
+     )
+}
